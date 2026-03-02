e5930af4a068c0cd5f39b6ccd553a1d5de4f3f83
Maria S <mariz87@yandex.ru>
2026-03-02 11:26:43 +0300

    Fix the link to JS file in service.html

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,12 @@
+// Находим кнопку
+const addCategoryBtn = document.getElementById('add-category');
+
+// Вешаем слушатель
+addCategoryBtn.addEventListener('click', function() {
+  // Здесь будет код, который выполнится при клике
+  console.log('Кнопка нажата!');
+});
 
 const serviceBlock = document.querySelector('.service');
 serviceBlock.hidden = false;
+
