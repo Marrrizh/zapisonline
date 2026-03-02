a355d499116c5103f6819c4d739f68c47e4d454d
Maria S <mariz87@yandex.ru>
2026-03-02 11:29:53 +0300

    Removing 'hidden' after clicking the button

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,9 @@
-// Находим кнопку
+
 const addCategoryBtn = document.getElementById('add-category');
 
-// Вешаем слушатель
 addCategoryBtn.addEventListener('click', function() {
-  // Здесь будет код, который выполнится при клике
-  console.log('Кнопка нажата!');
+  // Находим блок услуг
+  const serviceBlock = document.querySelector('.service');
+  // Убираем скрытие
+  serviceBlock.hidden = false;
 });
-
-const serviceBlock = document.querySelector('.service');
-serviceBlock.hidden = false;
-
